678d1310e84de2112c6d7490f70e51e98484306e
王涛 <oncwnuCuPnX9nPj-PtuHZ1bwuc1E@git.weixin.qq.com>
2020-08-14 17:34:42 +0800

    对平台管理接口，包括查询、新增、删除

---
+++ src/components/addPlatform/addPlatform.vue
@@ -0,0 +1,136 @@
+<template>
+  <div class="wtbody">
+    <el-dialog title="新建平台"  :visible.sync="dialogVisible" center :append-to-body='true' :before-close="handleClose" :lock-scroll="false" :close-on-click-modal="false" width="500px">
+      <el-form ref="form" :model="form" :rules="rules" label-width="80px">
+      	<el-form-item label="平台ip" prop="ip">
+      		<el-input v-model="form.ip"></el-input>
+      	</el-form-item>
+      	<el-form-item label="平台名称" prop="name">
+      		<el-input v-model="form.name"></el-input>
+      	</el-form-item>
+      	<el-form-item label="消息队列" prop="queue">
+      		<el-input v-model="form.queue"></el-input>
+      	</el-form-item>
+      </el-form>
+	  <div class="btnWrap">
+		  <el-button type="primary" @click="cancelBtn">取消</el-button>
+		  <el-button type="primary" @click="onSubmit">立即创建</el-button>
+	  </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+  import { addPlatform } from '@/api/api'
+  import {Debounce} from '@/utils/index.js'
+  export default{
+    data(){
+		var validateIP = (rule, value, callback) => {
+			if(value === ''){
+				callback(new Error('请输入IP'));
+			}else{
+				callback();
+			}
+		}
+		var validateQueue = (rule, value, callback) => {
+			if(value === ''){
+				callback(new Error('请输入消息队列'));
+			}else{
+				callback();
+			}
+		}
+		var validateName = (rule, value, callback) => {
+			if(value === ''){
+				callback(new Error('请输入平台名称'));
+			}else{
+				callback();
+			}
+		}
+		return{
+			dialogVisible:false,
+			form: {
+			  name: '',
+			  ip: '',
+			  queue: ''
+			},
+			rules:{
+			  ip:[
+				{ required: true,validator: validateIP, trigger: 'blur' }
+			  ],
+			  queue:[
+				{ required: true,validator: validateQueue, trigger: 'blur' }
+			  ],
+			  name:[
+				{ required: true,validator: validateName, trigger: 'blur' }
+			  ]
+			}
+      }
+    },
+    methods:{
+      // 下载模板
+      
+		handleClose(){
+			this.dialogVisible = false
+			this.form = {
+				name: '',
+				ip: '',
+				queue: ''
+			}
+			this.$nextTick(()=>{
+                 this.$refs.form.clearValidate();
+            })
+		},
+		onSubmit:Debounce (function(){
+			 this.$refs.form.validate((valid) => {
+					if (valid) {
+						let data = this.form
+						this.$confirm('确认添加新平台吗?', '提示', {
+						          confirmButtonText: '确定',
+						          cancelButtonText: '取消',
+						          type: 'warning'
+						        }).then(() => {
+								  addPlatform(data).then((res)=>{
+								  	console.log(res)
+									this.$message({
+									  type: 'success',
+									  message: '添加成功!'
+									});
+									this.$emit('refreshPlatform',1)
+								  }).catch((err)=>{
+									  this.$message.error('添加失败');
+								  })
+						        }).catch(() => {
+						          this.$message({
+						            type: 'info',
+						            message: '已取消添加'
+						          });          
+						        });
+					}
+			 })
+			
+		},300),
+		cancelBtn(){
+			this.dialogVisible = false
+			this.form = {
+				name: '',
+				ip: '',
+				queue: ''
+			}
+			this.$nextTick(()=>{
+                    this.$refs.form.clearValidate();
+                })
+		}
+    },
+    mounted(){
+
+    },
+  }
+</script>
+
+<style scoped lang="scss">
+  .btnWrap{
+	  display: flex;
+	  align-items: center;
+	  justify-content: flex-end;
+  }
+</style>
--- src/view/home/home.vue
+++ src/view/home/home.vue
@@ -22,7 +22,7 @@
                       </div>
                     </div>
                     <div class="selectItem1">
-                      <el-button type="primary" >新增</el-button>
+                      <el-button type="primary" @click="addPlatform">新增</el-button>
                       <el-button type="danger" @click="deletePlatform" :disabled="this.sels.length===0">删除</el-button>
                     </div>
                 </div>
@@ -70,6 +70,7 @@
         </div>
     <!-- </el-scrollbar> -->
     <dialog-platform ref="dialogPlatform"></dialog-platform>
+	<Add-Platform ref="addPlatform" @refreshPlatform="refreshPlatform"></Add-Platform>
   </div>
 </template>
 
@@ -77,11 +78,14 @@
   // import WtInput from '../../components/input.vue'
   import WtHeader from '@/components/myHeader/myHeader.vue'
   import DialogPlatform from '@/components/dialogPlatform/dialogPlatform.vue'
+  import AddPlatform from '@/components/addPlatform/addPlatform.vue'
+  import { getPlatform, deletePlatform } from '@/api/api'
   export default{
     name:'home',
     components:{
       WtHeader,
-      DialogPlatform
+      DialogPlatform,
+	  AddPlatform
     },
     data(){
       return{
@@ -92,57 +96,12 @@
             pageSize:20,
             tableTitle:[
                 { title : "平台名称", name : "name", type:"input",minwidth:'150'},
-                { title : "状态", name : "state", type:"input",minwidth:'120'},
+                { title : "状态", name : "status", type:"input",minwidth:'120'},
                 { title : "查看级联配置", type : "handle",button:[],width:'120' }
             ],
             //0正常 1异常
             tableData:[
-              {
-                name:'南宁总局',
-                state:'0'
-              },
-              {
-                name:'西乡塘分局',
-                state:'1'
-              },
-              {
-                name:'永宁分局',
-                state:'1'
-              },
-              {
-                name:'哈哈哈',
-                state:'0'
-              },
-              {
-                name:'梧州总局',
-                state:'1'
-              },
-              {
-                name:'青秀分局',
-                state:'0'
-              }
-              // {
-              //   name:'阿打算是',
-              //   state:'1'
-              // },
-              // {
-              //   name:'南宁群总局',
-              //   state:'1'
-              // },{
-              //   name:'阿斯蒂芬',
-              //   state:'0'
-              // },
-              // {
-              //   name:'南宁总阿斯顿发生局',
-              //   state:'1'
-              // },{
-              //   name:'维权群翁',
-              //   state:'0'
-              // },
-              // {
-              //   name:'南宁购物网点总局',
-              //   state:'1'
-              // }
+              
             ],
             tableAllData: [],
             clientHeight:'',
@@ -153,11 +112,11 @@
                 },
                 {
                 value: '1',
-                label: '正常'
+                label: '使用'
                 },
         		{
-                value: '0',
-                label: '异常'
+                value: '2',
+                label: '停用'
                 },
             ],
             valueW:"",
@@ -165,13 +124,8 @@
       }
     },
     methods:{
-      //回到上一页
-      backpage(){
-          this.$router.go(-1);
-      },
       //性别显示转换
       formatSex: function (row, column) {
-                //console.log(column.property)
                 if(column.property=="sex")
                 return row.sex == 1 ? '男' : row.sex == 0 ? '女' : '';
                 else
@@ -180,12 +134,8 @@
       arrFormatter (value,name) {
            if(name=='sex')
            return value == 1 ? '男' : value == 0 ? '女' : '';
-          else if(name=='multiplexMark')
-           return value == 1 ? '是' : value == 0 ? '否' : '';
-          else if(name=='isEnable')
-           return value == 1 ? '<span style="color:rgb(112, 182, 3);font-weight:bold">是</span>' :( value == 2 ? '<span style="color:#909399;font-weight:bold">否</span>' : ( value == 3 ? '<span style="color:#67C23A;font-weight:bold">进行中</span>' : ( value == 4 ? '<span style="color:#909399;font-weight:bold">已结束</span>' : '')));
-          else if(name == 'state')
-            return value == 0 ? '<span style="color:#f79898;font-weight:bold">异常</span>' :( value == 1 ? '<span style="color:rgb(112, 182, 3);font-weight:bold">正常</span>' : '');
+          else if(name == 'status')
+            return value == 2 ? '<span style="color:#f79898;font-weight:bold">停用</span>' :( value == 1 ? '<span style="color:rgb(112, 182, 3);font-weight:bold">使用</span>' : '');
           else
            return value;
       },
@@ -195,154 +145,86 @@
       handleSizeChange(val){
       	this.pageSize = val
       },
-      getEnrollData(){
-          this.listLoading=true
-          getUserList().then(res=>{
-              if(res.code==0){
-                  this.listLoading=false
-                  //console.log(res)
-                  this.tableAllData=res.data.data
-                  this.tableData=this.tableAllData
-              }else{
-                  this.listLoading=false
-                 this.$notify({
-                      title: '错误',
-                      message: res.msg,
-                      type: 'error'
-                  });
-              }
-          }).catch(err=>{
-              this.listLoading=false
-              this.$notify({
-                      title: '错误',
-                      message: err.msg,
-                      type: 'error'
-                  });
-          })
-          //this.tableData = JSON.parse(JSON.stringify(this.tableAllData))
+	  //获取到平台信息
+      getPlatform(){
+        getPlatform().then((res)=>{
+			if(res.code == 0){
+				let tableAllData = res.data.data
+				this.tableData = tableAllData
+				this.tableAllData = this.tableData
+			}
+		}).catch((err)=>{
+			console.log(err)
+		})
       },
+	  //新增平台
+	  addPlatform(){
+		this.$refs.addPlatform.dialogVisible = true
+	  },
+	  //通过状态进行筛选
       changeResultW(val){
-          //console.log(this.valueW)
           this.tableData = this.tableAllData.filter(item=>{
-              return String(item.isEnable).indexOf(val) > -1
+              return String(item.status).indexOf(val) > -1
           })
-      },
-      //修改
-      handleEdit(index,row){
-      	// //console.log(row)
-      	this.$refs.userData.setShow(row)
       },
       // 批量选中
       selsChange(sels){
           this.sels = sels
       },
       select(selection, row){
-         if (selection.length > 1) {
-
-                let del_row = selection.shift();
-
-                // console.log('把数组的第一个元素从其中删除后', selection);
-
-                this.$refs.table.toggleRowSelection(del_row, false);
-                // 用于多选表格，切换某一行的选中状态，如果使用了第二个参数，则是设置这一行选中与否（selected 为 true 则选中）
-
-            }
+        if(selection.length > 1) {
+	        let del_row = selection.shift();
+	        // console.log('把数组的第一个元素从其中删除后', selection);
+	        this.$refs.table.toggleRowSelection(del_row, false);
+			// 用于多选表格，切换某一行的选中状态，如果使用了第二个参数，则是设置这一行选中与否（selected 为 true 则选中）
+	    }
       },
       selectAll(selection) {
-
           // 选择项大于2时
-
           if (selection.length > 1) {
-
               selection.length = 1;
-
           }
-
       },
       getRowKeys(row) {
           return row.name;
       },
       //删除
-      handleCheck(index,row){
-      	let param = {
-      		id : row.id
-      	}
-      	//console.log(param)
-      	this.$confirm('此操作将永久删除该平台, 是否继续?', '提示', {
-      	  confirmButtonText: '确定',
-      	  cancelButtonText: '取消',
-      	  type: 'warning'
-      	}).then(() => {
-      	  deleteUser(param).then((res)=>{
-      		  if(res.code == 0){
-      			  this.$message({
-      				type: 'success',
-      				message: '删除成功!'
-      			  });
-      			  this.getEnrollData()
-      		  }else{
-      			  this.$message.error('删除失败');
-      		  }
-      	  }).catch((res)=>{
-      		  this.$message.error('删除失败');
-      	  })
-
-      	}).catch(() => {
-      	});
-      },
       deletePlatform(){
-        const h = this.$createElement;
-          this.$msgbox({
-            title: '警告',
-            message: h('p', null, [
-              h('span', null, '确认删除此平台吗？'),
-              // h('i', { style: 'color: teal' }, 'VNode')
-            ]),
-            showCancelButton: true,
-            confirmButtonText: '确定',
-            cancelButtonText: '取消',
-            beforeClose: (action, instance, done) => {
-              if (action === 'confirm') {
-                instance.confirmButtonLoading = true;
-                instance.confirmButtonText = '删除中...';
-                setTimeout(() => {
-                  done();
-                  setTimeout(() => {
-                    instance.confirmButtonLoading = false;
-                  }, 300);
-                }, 3000);
-              } else {
-                done();
-              }
-            }
-          }).then(action => {
-            this.$message({
-              type: 'info',
-              message: 'action: ' + action
-            });
-          });
-      },
-      //新增用户
-      addUser(){
-      	let val = 1
-      	// //console.log(val)
-      	this.$refs.userData.setShow(val)
-      },
-      // 更新页面
-      updateMess(val){
-      	this.getEnrollData()
-      },
-      //将tabledata的值传给tableAllData(到真正对接时就不用)
-      getTableAllData(){
-        this.tableAllData = this.tableData
+		let data = {
+			id:this.sels[0].id
+		}
+        this.$confirm('确认删除此平台吗?', '提示', {
+                  confirmButtonText: '确定',
+                  cancelButtonText: '取消',
+                  type: 'warning'
+                }).then(() => {
+					deletePlatform(data).then((res)=>{
+						this.$message({
+						  type: 'success',
+						  message: '删除成功!'
+						});
+						this.getPlatform()
+					}).catch((err)=>{
+					  this.$message.error('删除失败');
+					})
+                }).catch(() => {
+                  this.$message({
+                    type: 'info',
+                    message: '已取消删除'
+                  });          
+                });
       },
       //查看级联配置
       searchPlatform(index,row){
         this.$refs.dialogPlatform.dialogPassVisible = true
-      }
+      },
+	  //刷新页面
+	  refreshPlatform(val){
+		  this.getPlatform()
+	  }
     },
     mounted(){
-      this.getTableAllData()
+	  this.getPlatform()
     },
     computed:{
       tables:function(){
